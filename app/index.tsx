6f2d9a611010506d5dfb65111b2d1916f464c5b2
Peter Skaltsis <4243672+peterjskaltsis@users.noreply.github.com>
2023-05-27 15:20:30 +1000

    fix: tamagui 1.30

--- app/index.tsx
+++ app/index.tsx
@@ -6,32 +6,27 @@ import { useFonts } from "expo-font";
 import config from "../tamagui.config";
 
 export default function App() {
-	const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme();
 
-	const [loaded] = useFonts({
-		Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
-		InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
-	});
+  const [loaded] = useFonts({
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  });
 
-	if (!loaded) {
-		return null;
-	}
+  if (!loaded) {
+    return null;
+  }
 
-	return (
-		<TamaguiProvider config={config}>
-			<Theme name={colorScheme === "dark" ? "dark" : "light"}>
-				<YStack
-					f={1}
-					jc='center'
-					ai="center"
-					backgroundColor={"$backgroundSoft"}
-				>
-					<Paragraph color='$color' jc="center">
-						{colorScheme}
-					</Paragraph>
-					<StatusBar style="auto" />
-				</YStack>
-			</Theme>
-		</TamaguiProvider>
-	);
+  return (
+    <TamaguiProvider config={config}>
+      <Theme name={colorScheme === "dark" ? "dark" : "light"}>
+        <YStack f={1} jc="center" ai="center" backgroundColor="$background">
+          <Paragraph color="$color" jc="center">
+            {colorScheme}
+          </Paragraph>
+          <StatusBar style="auto" />
+        </YStack>
+      </Theme>
+    </TamaguiProvider>
+  );
 }
